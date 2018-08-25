6d51b19c86ea7d71b0bc48579b50bdd289d62060
Ian Richard <ianrichard03@gmail.com>
2018-08-24 23:43:47 -0500

    added js to display score and get key strokes.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,19 +3,36 @@
 //on click game begins, start button is now a restart button.
 function gameStart(){
     
-        
+    //display score board and change button tag.
     document.getElementById("message").style.visibility = "visible";
     document.getElementById("start").innerHTML = "Restart";
     document.getElementById("start").id = "restart";
-    document.getElementById("guessMe").innerHTML = "FROM RUSSIA WITH LOVE";
     
-    
-    
-    //
+
+    //update scorebox with a loop instead of 3 statements. 
+    var scoreBox = ["left", "middle", "right"];
+
+    for(var i = 0; i < scoreBox.length; i++){
+        document.getElementById(scoreBox[i]).style.visibility = "visible";
+    }
+
+    //set initial score and tries.
     var tries = 5;
     var score = 0;
+    var letters = ["","","","",""];
     
+    document.getElementById("tries").innerHTML = "Tries: " + tries;
+    document.getElementById("score").innerHTML = "Score: " + score;
    
+    document.onkeyup = function(event) {
+    
+        for(var x = 0; x <= tries; x++ ){
+        letters[x] = event.key;
+        }
+        
+        document.getElementById("letters").innerHTML = "Letters: " + letters[0] + " " + letters[1] + " " + letters[2] + " " + letters[3] + " " + letters[4];
+    }
     
    
 }
+
