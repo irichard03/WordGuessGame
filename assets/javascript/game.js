411abd964cfdaf7664cbb741cb5f5776532c77d7
Ian Richard <ianrichard03@gmail.com>
2018-08-24 21:39:08 -0500

    added gameStart() and change #start to #restart makes #message visible.

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+
+
+//on click game begins, start button is now a restart button.
+function gameStart(){
+    
+        
+    document.getElementById("message").style.visibility = "visible";
+    document.getElementById("start").innerHTML = "Restart";
+    document.getElementById("start").id = "restart";
+    
+    //Game lives in this for loop, exit condition is x = -1
+    
+    
+    
+   
+}
