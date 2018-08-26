1c3bffcaf631fcbf0db99a872b41a5763819cf78
Ian Richard <ianrichard03@gmail.com>
2018-08-26 00:10:43 -0500

    added wordpbank, getRandomINt function to pick random word from wordbank and display in #guessMe

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,23 +16,40 @@ function gameStart(){
         document.getElementById(scoreBox[i]).style.visibility = "visible";
     }
 
-    //set initial score and tries.
+    //set initial score and tries. Create letters array and word bank array.
     var tries = 5;
     var score = 0;
-    var letters = ["","","","",""];
-    
+    var letters = [];
+    //21 entries 
+    var wordBank = ["TOLSTOY","MOSCOW","STALIN","LENNIN","STALINGRAD","LENNINGRAD","KALISHNAKOV","YAKOLEV","VODKA","TOKAREV","GREGARIN","TERESHKOVA","COSMONAUT","SPUTNIK","RASPUTIN","CATHERINE","DOSTOEVSKY","PASTERNAK","URAL","MIKOYAN","TUPOLOV","POTEMKIN","BAKUNIN","DRAGO","IVAN","SHARAPOVA","GLASNOST","TZAR","SOYUZ","MIR","BABAYAGA"];
+    var target;
+    var choice;
+    var word = "Letters: ";
+    var playing = true;
+
     document.getElementById("tries").innerHTML = "Tries: " + tries;
     document.getElementById("score").innerHTML = "Score: " + score;
-   
-    document.onkeyup = function(event) {
     
-        for(var x = 0; x <= tries; x++ ){
-        letters[x] = event.key;
-        }
+    //math random function to get wordbank word and assign to target in gamw word choice loop.
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+      }
+
+    document.onkeyup = function(event) {
+                
+                //Change guessMe to one of the words in the wordbank.
+                target = getRandomInt(28);
+                document.getElementById("guessMe").innerHTML = wordBank[target];    
+
+               
+                    
+                choice = event.key;    
+                letters.push(choice);
+                document.getElementById("letters").innerHTML = word + letters;
+                document.getElementById("tries").innerHTML = "Tries: " + tries;
+            
         
-        document.getElementById("letters").innerHTML = "Letters: " + letters[0] + " " + letters[1] + " " + letters[2] + " " + letters[3] + " " + letters[4];
     }
     
    
 }
-
