c8c52ddb3d4b466af1c742c15c47ca43914404a9
Ian Richard <ianrichard03@gmail.com>
2018-08-27 22:08:22 -0500

    picked random word once, instead of each time letter is selected

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,13 +3,14 @@
 //on click game begins, start button is now a restart button.
 function gameStart(){
     
-    //display score board and change button tag.
+    //display score board and change start button to restart button tag.
     document.getElementById("message").style.visibility = "visible";
     document.getElementById("start").style.display = "none";
     document.getElementById("restart").style.display = "inline-block";
-    
+    target = getRandomInt(28);
+                
 
-    //update scorebox with a loop instead of 3 statements. 
+    //update scorebox with a loop instead of 3 statements, mostly because I couldn't figure out foreach.
     var scoreBox = ["left", "middle", "right"];
 
     for(var i = 0; i < scoreBox.length; i++){
@@ -20,7 +21,7 @@ function gameStart(){
     var tries = 5;
     var score = 0;
     var letters = [];
-    //21 entries 
+    //21 entries apparently all I know about russia is from WW2, their Space program, Counterstrike and literature I haven't red.""
     var wordBank = ["TOLSTOY","MOSCOW","STALIN","LENNIN","STALINGRAD","LENNINGRAD","KALISHNAKOV","YAKOLEV","VODKA","TOKAREV","GREGARIN","TERESHKOVA","COSMONAUT","SPUTNIK","RASPUTIN","CATHERINE","DOSTOEVSKY","PASTERNAK","URAL","MIKOYAN","TUPOLOV","POTEMKIN","BAKUNIN","DRAGO","IVAN","SHARAPOVA","GLASNOST","TZAR","SOYUZ","MIR","BABAYAGA"];
     var target;
     var choice;
@@ -30,7 +31,7 @@ function gameStart(){
     document.getElementById("tries").innerHTML = "Tries: " + tries;
     document.getElementById("score").innerHTML = "Score: " + score;
     
-    //math random function to get wordbank word and assign to target in gamw word choice loop.
+    //math random function to get wordbank word and assign to target in game word choice loop.
     function getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max));
       }
@@ -38,8 +39,16 @@ function gameStart(){
     document.onkeyup = function(event) {
                 
                 //Change guessMe to one of the words in the wordbank.
-                target = getRandomInt(28);
-                document.getElementById("guessMe").innerHTML = wordBank[target];    
+                
+                for(var blanks = 0; blanks <= wordBank[target].length; blanks++){
+                
+                document.getElementById("guessMe").innerHTML = wordBank[target];
+
+                }
+                
+                
+                
+
 
                
                     
